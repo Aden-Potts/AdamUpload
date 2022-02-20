5e3b6632fbb9ebef6ee41b54f9239a82a69c8ed9
Aden Potts <adenpotts01@icloud.com>
2022-02-20 05:11:08 -0500

    Remove useless Users array in $Cfg

--- inc/config.example.php
+++ inc/config.example.php
@@ -3,9 +3,6 @@
 $Cfg = [
     "Domain" => "https://files.adamscodebox.com",
     "UploadDir" => "", // Add a trailing slash, unless you want the upload dir to be the current directory.
-    "Users" => [
-        "apikey" => "username here" // will be removed soon in favor for an actual logon system.
-    ],
     "WhitelistedExtensions" => ["jpg", "jpeg", "png", "gif"],
     "DB" => [
         "Host" => "127.0.0.1",
