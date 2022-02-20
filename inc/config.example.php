f91ad78e2253dfe68ba91c152866804fe4959254
Aden Potts <adenpotts01@icloud.com>
2022-02-19 21:55:29 -0500

    Update config.example.php to fit the actual config.

--- inc/config.example.php
+++ inc/config.example.php
@@ -1,12 +1,16 @@
 <?php
 
-require_once("functions.php");
-
 $Cfg = [
     "Domain" => "https://files.adamscodebox.com",
-    "UploadDir" => "", // Add a trailing slash, unless you want the upload dir to be the correct directory.
+    "UploadDir" => "", // Add a trailing slash, unless you want the upload dir to be the current directory.
     "Users" => [
-        "keyhere" => "username here"
+        "apikey" => "username here" // will be removed soon in favor for an actual logon system.
     ],
-    "WhitelistedExtensions" => ["jpg", "jpeg", "png", "gif"]
+    "WhitelistedExtensions" => ["jpg", "jpeg", "png", "gif"],
+    "DB" => [
+        "Host" => "127.0.0.1",
+        "User" => "root",
+        "Password" => "",
+        "Database" => "adamupload"
+    ]
 ];
